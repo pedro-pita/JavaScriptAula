b912a2c320b968c7c9ab22ad56b6b6829651d62a
pedro-pita <hpp.henriquepita@gmail.com>
2018-01-23 12:06:48 +0000

    Aula 4 e 5
    Acabmos a Aula 4 e começaos a aula 5

---
+++ js/aula5.js
@@ -0,0 +1,76 @@
+/*var idade = "20";
+//alert(idade.length);
+//alert(new String(idade).length);
+var nome = new String("Sergio");
+console.log(nome);
+//alert(nome.valueOf());
+
+var num1 = new Number(1.120);
+//alert(typeof(num1));
+var n = new Number();
+document.write("<br/> Valor de n "+n);
+var n1 = new Number("Teste");
+document.write("<br/> Valor de n1 "+n1)
+var n2 = new Number("123");
+document.write("<br/> Valor de n2 "+n2);
+var n3 = new Number(123434);
+document.write("<br/> Valor de n2 "+n2);
+var n4 = new Number(false);
+document.write("<br/> Valor de n4 "+n4);
+var n5 = new Number(true);
+document.write("<br/> Valor de n5 "+n5);
+var n6 = new Number(1234);
+var n7 = new Number(n6);
+document.write("<br/> Valor de n6 "+n6);
+document.write("<br/> Valor de n7 "+n7);
+
+document.write("<br/> Valor de NaN "+Number.NaN);
+document.write("<br/> Valor de MAX_VALUE "+Number.MAX_VALUE);
+document.write("<br/> Valor de MIN_VALUE "+Number.MIN_VALUE);
+document.write("<br/> Valor de NEGATIVE_INFINITY "+Number.NEGATIVE_INFINITY);
+document.write("<br/> Valor de POSITIVE_INFINITY "+Number.POSITIVE_INFINITY);
+var numero = new Number("10.12345");
+document.write("<br/>Valor de numero "+numero.toFixed());
+document.write("<br/>Valor de numero "+numero.toFixed(3));
+document.write("<br/>Valor de numero "+numero.toPrecision());
+document.write("<br/>Valor de numero "+numero.toPrecision(1));
+document.write("<br/>Valor de numero "+numero.toString());*/
+
+var exemplo = "JavaScript";
+document.write("Texto escrito com o metodo big->  "+exemplo.big()+"<br/>");
+
+document.write("Texto escrito com o metodo bold->  "+exemplo.bold()+"<br/>");
+
+document.write("Texto escrito com o metodo italics->  "+exemplo.italics()+"<br/>");
+
+document.write("Texto escrito com o metodo small->  "+exemplo.small()+"<br/>");
+
+document.write("Texto escrito com o metodo strike->  "+exemplo.strike()+"<br/>");
+
+document.write("Texto escrito com o metodo sup->  "+exemplo.sup()+"<br/>");
+
+document.write("Texto escrito com o metodo sub->  "+exemplo.sub()+"<br/>");
+
+document.write("Texto escrito com o metodo toLowerCase->  "+exemplo.toLowerCase()+"<br/>");
+
+document.write("Texto escrito com o metodo toUpperCase->  "+exemplo.toUpperCase()+"<br/>");
+
+document.write("Texto escrito com o metodo substring(0,4)->  "+exemplo.substring(0,4)+"<br/>");
+
+document.write("Texto escrito com o metodo substring(4)->  "+exemplo.substring(4)+"<br/>");
+
+document.write("Texto escrito com o metodo substring(4,2)->  "+exemplo.substring(4,2)+"<br/>");
+
+document.write("Texto escrito com o metodo substring(2,4)->  "+exemplo.substring(2,4)+"<br/>");
+
+var str = "Sergio";
+document.write("Texto escrito com o metodo charAt(str.length - 1)->  "+str.charAt(str.length - 1)+"<br/>");
+
+document.write("Texto escrito com o metodo charAt(3)->  "+str.charAt(3)+"<br/>");
+
+//codigo tabela ascci
+document.write("Texto escrito com o metodo charCodeAt(3)->  "+str.charCodeAt(3)+"<br/>");
+
+
+
+
